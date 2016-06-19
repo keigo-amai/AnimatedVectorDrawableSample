ee7b6ada2c2535c05e8b322594419dc67c8b38a1
keigo.amai <amaikeigo.10@gmail.com>
2016-06-20 01:49:28 +0900

    リバース画像も勝手に指定するカスタムアトリビュート

--- app/src/main/java/com/amadroid/vectoranimationsample/view/AnimationButton.java
+++ app/src/main/java/com/amadroid/vectoranimationsample/view/AnimationButton.java
@@ -1,6 +1,7 @@
 package com.amadroid.vectoranimationsample.view;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.drawable.AnimatedVectorDrawable;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
@@ -10,14 +11,31 @@ import android.widget.Button;
 import com.amadroid.vectoranimationsample.R;
 
 /**
+ * 押したらベクターアニメーションがスタートするView
+ * app:ResourceNameでdrawableリソース名を指定したら勝手にリバースも指定してくれる.
+ *
  * Created by Keigo Amai on 2016/06/20.
  */
 public class AnimationButton extends Button implements View.OnClickListener {
+
+    private static final String REVERSE_SUFFIX = "_reverse";
+    private String mResName;
 
     public AnimationButton(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
 
         setOnClickListener(this);
+
+        TypedArray a = context.getTheme().obtainStyledAttributes(
+                attrs,
+                R.styleable.AnimationButton,
+                0, 0);
+
+        try {
+            mResName = a.getString(R.styleable.AnimationButton_ResourceName);
+        } finally {
+            a.recycle();
+        }
     }
 
     public AnimationButton(Context context, AttributeSet attrs) {
@@ -37,7 +55,8 @@ public class AnimationButton extends Button implements View.OnClickListener {
             if (drawable instanceof AnimatedVectorDrawable) {
                 if (((AnimatedVectorDrawable) drawable).isRunning()) return;
 
-                int drawableResId = button.isSelected() ? R.drawable.fill_in_heart : R.drawable.fill_in_heart_reverse;
+                String stateResName = button.isSelected() ? mResName + REVERSE_SUFFIX : mResName;
+                int drawableResId = getResources().getIdentifier(stateResName, "drawable", getContext().getPackageName());
                 button.setBackground(getContext().getDrawable(drawableResId));
 
                 drawable = button.getBackground();
